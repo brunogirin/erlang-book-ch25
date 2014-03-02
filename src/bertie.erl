35f1baca232947270f21d64d585d37e1414fb2eb
Bruno Girin <brunogirin@gmail.com>
2014-03-02 11:25:34 +0000

    Made the app use bitcask and created related rebar.config.

--- src/bertie.erl
+++ src/bertie.erl
@@ -9,10 +9,25 @@
 %% ====================================================================
 -export([start/0]).
 
-start() -> io:format("Hello my name is Bertie~n").
+start() ->
+    Handle = bitcask:open("bertie_database", [read_write]),
+    N = fetch(Handle),
+    store(Handle, N+1),
+    io:format("Bertie has been run ~p times~n", [N]),
+    bitcask:close(Handle),
+    init:stop().
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+
+store(Handle, N) ->
+    bitcask:put(Handle, <<"bertie_executions">>, term_to_binary(N)).
+
+fetch(Handle) ->
+    case bitcask:get(Handle, <<"bertie_executions">>) of
+        not_found -> 1;
+        {ok, Bin} -> binary_to_term(Bin)
+    end.
 
 
